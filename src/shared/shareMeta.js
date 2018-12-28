d07181719b88578e87e5929c33093c9d7c095044
Your Name <you@example.com>
2018-12-28 16:42:25 +0800

    build

--- src/shared/shareMeta.js
+++ src/shared/shareMeta.js
@@ -1,10 +1,10 @@
 import config from '~/config'
 /* eslint-disable import/no-webpack-loader-syntax */
-import img from '!!file-loader!~/images/logo.png'
+// import img from '!!file-loader!~/images/logo.png'
 
 export default {
-  title: 'title',
-  desc: 'blah...blah...blah...',
-  imgUrl: img,
+  title: '你有一份择机而动的见识宝典待领取 or 就在见识！必须献给2019的宝典',
+  desc: '见识过2018的风起云涌，你的2019必须择机而动',
+  imgUrl: 'https://wpimg.wallstcn.com/834f39db-37b6-4297-9731-cf3e3b927bfe.png',
   link: `${config.BASE_URL}/`
 }
