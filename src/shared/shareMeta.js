8534f5d14f5d0623134a3c6f112c26a6f5dc9874
nicholasxjy <yitaqi2011@gmail.com>
2018-12-29 10:22:23 +0800

    fix url link

--- src/shared/shareMeta.js
+++ src/shared/shareMeta.js
@@ -4,7 +4,7 @@
 import { wechatSignature } from '../services/wits'
 
 const shareData = {
-  title: '你有一份择机而动的见识宝典待领取 or 就在见识！必须献给2019的宝典',
+  title: '你有一份择机而动的见识宝典待领取',
   desc: '见识过2018的风起云涌，你的2019必须择机而动',
   imgUrl: 'https://wpimg.wallstcn.com/834f39db-37b6-4297-9731-cf3e3b927bfe.png',
   link: location.href
