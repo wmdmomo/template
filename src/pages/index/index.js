769628f37a0059316553ca168a603d5bb75dc14d
nicholasxjy <yitaqi2011@gmail.com>
2019-01-16 12:20:04 +0800

    build

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -11,14 +11,95 @@ import { submitRecruit } from '../../services/wits'
 function isPhone(phone) {
   return /^1\d{10}$/.test(phone)
 }
+
 if (window.wx) {
   setWechatShare()
 }
+
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d / 2
+  if (t < 1) {
+    return (c / 2) * t * t + b
+  }
+  t--
+  return (-c / 2) * (t * (t - 2) - 1) + b
+}
+
+Math.easeInCubic = function (t, b, c, d) {
+  const tc = (t /= d) * t * t
+  return b + c * tc
+}
+
+Math.inOutQuintic = function (t, b, c, d) {
+  let ts = (t /= d) * t
+
+  let tc = ts * t
+  return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc)
+}
+
+// requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
+var requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || function (callback) {
+      window.setTimeout(callback, 1000 / 60)
+    }
+  )
+})()
+
+function scrollTo(to, callback, duration) {
+  // because it's so fucking difficult to detect the scrolling element, just move them all
+  function move(amount) {
+    document.documentElement.scrollTop = amount
+    document.body.parentNode.scrollTop = amount
+    document.body.scrollTop = amount
+  }
+  function position() {
+    return (
+      document.documentElement.scrollTop
+      || document.body.parentNode.scrollTop
+      || document.body.scrollTop
+    )
+  }
+  let start = position()
+
+  let change = to - start
+
+  let currentTime = 0
+
+  let increment = 20
+  duration = typeof duration === 'undefined' ? 500 : duration
+  const animateScroll = function () {
+    // increment the time
+    currentTime += increment
+    // find the value with the quadratic in-out easing function
+    const val = Math.easeInOutQuad(currentTime, start, change, duration)
+    // move the document.body
+    move(val)
+    // do the animation unless its over
+    if (currentTime < duration) {
+      requestAnimFrame(animateScroll)
+    } else {
+      if (callback && typeof callback === 'function') {
+        // the animation is done so lets callback
+        callback()
+      }
+    }
+  }
+  animateScroll()
+}
+
 let isFetching = false
 
 document.addEventListener('DOMContentLoaded', () => {
   const fetchbutton = document.querySelector('.bottom')
   fetchbutton.addEventListener('click', async () => {
+    // scroll to bottom first
+    const height = Math.max(document.body.scrollHeight, document.body.offsetHeight,
+      document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight)
+    scrollTo(height)
     const phonenumber = document.getElementById('field1').value
     const weixin = document.getElementById('field2').value
     const weibo = document.getElementById('field3').value
